2cd4f73a561a4fc5f98559bb0d55861d89c9ea0d
moeyue23 <185361175+moeyue23@users.noreply.github.com>
2026-02-09 20:46:16 +0800

    feat(theme): add dark mode support

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import './i18n';
 import './index.css';
+import { ThemeProvider } from '@/components/theme-provider';
+
 import Landing from './pages';
 
 const router = createBrowserRouter([{ path: '/', element: <Landing /> }]);
@@ -11,6 +13,8 @@ const root = document.getElementById('root') as HTMLDivElement;
 
 createRoot(root).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 );
